d61d4dcd1813701ee00c916280d105bef4e1ae09
mkiam <migan_karen@yahoo.fr>
2015-01-20 14:58:03 +0100

    Mise en place

--- webserver/main.c
+++ webserver/main.c
@@ -2,13 +2,12 @@
 #include <string.h>
 int main (int argc,char ** argv)
 {
-/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-if (argc > 1 && strcmp (argv [1] ," - advice ") == 0) {
-printf (" Don ’t Panic !\ n ");
-return 42;
-}
-printf (" Need an advice ?\ n ");
-return 0;
+	if (argc > 1 && strcmp (argv [1] ,"-advice") == 0) {
+		printf ("Don't Panic !\n");
+		return 42;
+	}
+	printf (" Need an advice ?\n");
+	return 0;
 }
 
 
