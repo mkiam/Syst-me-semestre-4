5430ca191d01bce1c68c957a0b66f61bdd55aa4d
Deltour <lucas.deltour95@gmail.com>
2015-01-26 15:42:38 +0100

    ajout sleep

--- webserver/main.c
+++ webserver/main.c
@@ -13,34 +13,21 @@ int main (void/*int argc,char ** argv*/)
   int socket_client;
   while(1){
     socket_client = accept (socket_serveur, NULL, NULL );
-
-<<<<<<< HEAD
-  if (socket_client == -1)
-{
-perror ( "accept" );
-/* traitement d’ erreur */
-}
-/* On peut maintenant dialoguer avec le client */
-const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n" ;
-write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
- int nb=read(socket_client,buf, 256);
- while(nb>0){
-   write(socket_client,buf,nb);
-}
-=======
     if (socket_client == -1)
       {
 	perror ( "accept" );
 	/* traitement d’ erreur */
       }
     /* On peut maintenant dialoguer avec le client */
+    
+    sleep(1);
     const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n" ;
     write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
     int nb=read(socket_client,buf, 256);
-      while(nb>0){
-	write(socket_client,buf,nb);
-      }
->>>>>>> 86e9c35eafb9cbb0aaae8ce9bd0d78b558e9e8e2
+    while(nb>0){
+      write(socket_client,buf,nb);
+      nb=read(socket_client,buf, 256);
+    }
    
   }
   return 0;
