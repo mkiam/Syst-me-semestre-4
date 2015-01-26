86e9c35eafb9cbb0aaae8ce9bd0d78b558e9e8e2
Deltour <lucas.deltour95@gmail.com>
2015-01-26 15:19:59 +0100

    fix creer-serveur

--- webserver/main.c
+++ webserver/main.c
@@ -11,22 +11,22 @@ int main (void/*int argc,char ** argv*/)
   int socket_serveur = creer_serveur(8000);
   char buf[256];
   int socket_client;
-while(1){
-  socket_client = accept (socket_serveur, NULL, NULL );
+  while(1){
+    socket_client = accept (socket_serveur, NULL, NULL );
 
-  if (socket_client == -1)
-{
-perror ( "accept" );
-/* traitement d’ erreur */
-}
-/* On peut maintenant dialoguer avec le client */
-const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n" ;
-write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
-
- while(int nb=read(socket_client,buf, 256)>0){
-   write(socket_client,buf,nb);
-}
+    if (socket_client == -1)
+      {
+	perror ( "accept" );
+	/* traitement d’ erreur */
+      }
+    /* On peut maintenant dialoguer avec le client */
+    const char * message_bienvenue = " Bonjour , bienvenue sur mon serveur \n" ;
+    write ( socket_client , message_bienvenue , strlen ( message_bienvenue ));
+    int nb=read(socket_client,buf, 256);
+      while(nb>0){
+	write(socket_client,buf,nb);
+      }
    
-	}
- return 0;
+  }
+  return 0;
 }
